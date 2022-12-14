c4359a03ff523734009dd15341ff6e00e3d2191d
MichaelGustavsson <michael.gustavsson@softtech-dev.se>
2022-12-14 14:08:31 +0100

    Finalized simple tests for RegisterUser component

--- final/westcoast-cars-admin/src/Components/RegisterUser.test.js
+++ final/westcoast-cars-admin/src/Components/RegisterUser.test.js
@@ -4,7 +4,7 @@ import RegisterUser from './RegisterUser';
 
 describe('Register User Component', () => {
   describe('Register User Page', () => {
-    it('has a header saying Register User', () => {
+    test('has a title text saying Register User', () => {
       // Arrange...
       render(<RegisterUser />);
       const titleText = screen.getByRole('heading', {
@@ -14,6 +14,66 @@ describe('Register User Component', () => {
 
       // Assert...
       expect(titleText).toBeInTheDocument();
+    });
+
+    test('has an username input', () => {
+      // Arrange...
+      //   const { container } = render(<RegisterUser />);
+      //   const userInput = container.querySelector('input');
+
+      render(<RegisterUser />);
+      //   const userInput = screen.getByPlaceholderText('username');
+      const userInput = screen.getByLabelText('User Name');
+      // Act...
+
+      // Assert...
+      expect(userInput).toBeInTheDocument();
+    });
+
+    test('has an email input', () => {
+      // Arrange...
+      render(<RegisterUser />);
+      const emailInput = screen.getByLabelText('Email');
+      // Act...
+
+      // Assert...
+      expect(emailInput).toBeInTheDocument();
+    });
+
+    test('has a password input', () => {
+      // Arrange...
+      render(<RegisterUser />);
+      const passwordInput = screen.getByLabelText('Password');
+      // Act...
+
+      // Assert...
+      expect(passwordInput).toBeInTheDocument();
+      expect(passwordInput.type).toBe('password');
+    });
+
+    test('has a confirm password input', () => {
+      // Arrange...
+      render(<RegisterUser />);
+      const confirmPasswordInput = screen.getByLabelText('Confirm Password');
+
+      // Act...
+
+      //Assert...
+      expect(confirmPasswordInput).toBeInTheDocument();
+      expect(confirmPasswordInput.type).toBe('password');
+    });
+
+    test('should have a register button', () => {
+      // Arrange...
+      render(<RegisterUser />);
+      const registerButton = screen.getByRole('button', {
+        name: /Register User/i,
+      });
+
+      // Act...
+
+      // Assert...
+      expect(registerButton).toBeInTheDocument();
     });
   });
 });
